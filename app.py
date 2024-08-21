b8a32cea131734fb1d981bff9cc3f94a773e065b
Laureano Della Gaspera <ldellagaspera@solvencia.local>
2024-08-21 12:40:10 -0300

    agregar estructura de archivos

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask
+from flask import url_for
 from markupsafe import escape
+from flask import request
+from flask import render_template
+from flask import abort
+from flask import redirect
+from flask import session
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", body="index", title="index")
+
+@app.route("/login")
+def login():
+    return render_template("login/index.html", body="login", title="login")
+
+@app.route("/mercaderia")
+def mercaderia():
+    return render_template("mercaderia/index.html", body="mercaderia", title="mercaderia")
