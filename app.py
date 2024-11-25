21de300ac2a02ce97149f9d1b076488b4e96be1d
Silvio SM. Morales <smorales@solvencia.local>
2024-11-25 19:11:42 -0300

    agregue cambios a bloqueados

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ from routes import extracto
 from routes import index
 from routes import errors
 from routes import bloqueos
+from routes import hojalata
 from db import db
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex()
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1234@localhost:5432/laudos'
-app.config['SQLALCHEMY_BINDS'] = {
-    'sqlserver': 'mssql+pyodbc://arballon_RO:SolArb2024@Sql-server.solvencia.local/arballon?driver=ODBC+Driver+17+for+SQL+Server'
-}
+app.config['SQLALCHEMY_BINDS'] = {'sqlserver':'mssql+pyodbc://arballon_RO:SolArb2024@Sql-server.solvencia.local/arballon?driver=ODBC+Driver+17+for+SQL+Server'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.db.init_app(app)
 app.register_blueprint(login.login_bp)
@@ -31,3 +30,4 @@ app.register_blueprint(index.index_bp)
 app.register_blueprint(errors.errors_bp)
 app.register_blueprint(extracto.extracto_bp)
 app.register_blueprint(bloqueos.bloqueos_bp)
+app.register_blueprint(hojalata.hojalata_bp)
--- routes/bloqueos.py
+++ routes/bloqueos.py
@@ -3,32 +3,39 @@ from flask import redirect
 from flask import url_for
 from utils import helpers
 from flask import Blueprint
+from models import mod_hojalata
+from models import mod_mercaderia
+from flask import flash
+from flask import request
+from flask import session
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 bloqueos_bp = Blueprint("bloqueos", __name__)
 
-@bloqueos_bp.route("/bloqueos")
+@bloqueos_bp.get("/bloqueos")
 def bloqueos():
-    if helpers.session_on() and helpers.authorized_to("bloqueo"):
-        title = "bloqueados"
-        body = "bloqueos"
-        return render_template("bloqueados/index.html", title=title, body=body)
+    if helpers.session_on() and helpers.authorized_to("hojalata"):
+        title = "hojalata"
+        section = "hojalata"
+        return render_template("bloqueados/index.html", title=title, section=section)
     else:
         return redirect(url_for("login.login_get"))
+# @bloqueos_bp.route("/bloqueos/agregar")
+# def agregar():
+#     if helpers.session_on() and helpers.authorized_to("bloqueo"):
+#         title = "AGRAGAR BLOQUEO"
+#         body = "agregar"
+#         return render_template("bloqueados/agregar.html", title=title, body=body)
+#     else:
+#         return redirect(url_for("login.login_get"))
     
-@bloqueos_bp.route("/bloqueos/agregar")
-def agregar():
-    if helpers.session_on() and helpers.authorized_to("bloqueo"):
-        title = "AGRAGAR BLOQUEO"
-        body = "agregar"
-        return render_template("bloqueados/agregar.html", title=title, body=body)
-    else:
-        return redirect(url_for("login.login_get"))
-    
-@bloqueos_bp.route("/bloqueos/editar")
-def editar():
-    if helpers.session_on() and helpers.authorized_to("bloqueo"):
-        title = "MODIFICACION"
-        body = "editar"
-        return render_template("bloqueados/editar.html", title=title, body=body)
-    else:
-        return redirect(url_for("login.login_get"))
+# @bloqueos_bp.route("/bloqueos/editar")
+# def editar():
+#     if helpers.session_on() and helpers.authorized_to("bloqueo"):
+#         title = "MODIFICACION"
+#         body = "editar"
+#         return render_template("bloqueados/editar.html", title=title, body=body)
+#     else:
+#         return redirect(url_for("login.login_get"))
