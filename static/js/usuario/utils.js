99273bc5c6a87965f7311ada62efe923ff52f06a
Laureano Della Gaspera <ldellagaspera@solvencia.local>
2026-03-02 06:36:37 -0300

    mostrar device id

--- static/js/usuario/utils.js
+++ static/js/usuario/utils.js
@@ -1,11 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
+function getCookie(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? match[2] : null;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let device = document.getElementById("device");
+    
+    const savedDevice = getCookie("device_id");
+    if (savedDevice) {
+        device.value = savedDevice;
+    }
+
     const guardar = document.getElementById("guardar");
-    guardar.addEventListener("click", function(){
-        let device = document.getElementById("device").value;
-        if(device.length <= 12){
-            document.cookie = `device_id=${device}; max-age=31536000; path=/`;
-        }else{
-            alert('Utilice 12 caracteres o menos para identificar el dispositivo')
+    guardar.addEventListener("click", function () {
+        if (device.value.length <= 12) {
+            document.cookie = `device_id=${device.value}; max-age=31536000; path=/`;
+            alert("Dispositivo identificado correctamente");
+        } else {
+            alert('Utilice 12 caracteres o menos para identificar el dispositivo');
         }
-    })
-})
+    });
+});
